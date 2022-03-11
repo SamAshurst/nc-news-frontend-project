39699b930ada45b55fc85ee2739d9deefca13d0e
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-03-11 10:22:07 +0000

    Adding more spacing to query bar

--- src/components/QueryBar.jsx
+++ src/components/QueryBar.jsx
@@ -12,7 +12,7 @@ export default function QueryBar({defaultOrder}) {
         </Link>
         <span className="QueryBar-date">Date</span>
         <Link to={`/topics/${topic_slug}/?sort_by=created_at&order=asc`}>
-          <button className="QueryBar-button">
+          <button className="QueryBar-button-right">
             <i className="fa-solid fa-arrow-up-short-wide"></i>
           </button>
         </Link>
@@ -24,7 +24,7 @@ export default function QueryBar({defaultOrder}) {
         </Link>
         <span className="QueryBar-comments">Comments</span>
         <Link to={`/topics/${topic_slug}/?sort_by=comment_count&order=asc`}>
-          <button className="QueryBar-button">
+          <button className="QueryBar-button-right">
             <i className="fa-solid fa-arrow-up-short-wide"></i>
           </button>
         </Link>
@@ -36,7 +36,7 @@ export default function QueryBar({defaultOrder}) {
         </Link>
         <span className="QueryBar-votes">Votes</span>
         <Link to={`/topics/${topic_slug}/?sort_by=votes&order=asc`}>
-          <button className="QueryBar-button">
+          <button className="QueryBar-button-right">
             <i className="fa-solid fa-arrow-up-short-wide"></i>
           </button>
         </Link>
